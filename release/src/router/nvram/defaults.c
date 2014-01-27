9b6d130beeee26425bbec253a9b11e5fff339c7d
Jacky <jacky@prahec.com>
2014-01-27 22:29:03 +0100

    AdvancedTomato 1.20.16 - Change log coming soon!

--- release/src/router/nvram/defaults.c
+++ release/src/router/nvram/defaults.c
@@ -689,6 +689,10 @@ const defaults_t defaults[] = {
 	{ "tomatoanon_answer",		"0"				},
 	{ "tomatoanon_id",		""				},
 	{ "tomatoanon_notify",		"1"				},
+    
+// AdvancedTomato
+    { "at_update",    ""    },
+    { "at_width",    "fluid"    },
 
 #ifdef TCONFIG_USB
 // nas-usb - !!TB
